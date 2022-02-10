13e40412e3132eb411780a2312bd7726fce21450
Agathiyan <agathiyans28@gmail.com>
2022-02-10 23:43:26 +0530

    Navbar Completed..

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import { greeting } from "../../data";
+import "./Navbar.css";
+
+function Navbar() {
+  const links = [
+    { name: "Home", to: "/" },
+    { name: "Education", to: "/education" },
+    { name: "Projects", to: "/projects" },
+    { name: "About", to: "/about" }
+  ];
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-md container-fluid py-3">
+        <div className="container-fluid mx-lg-4 mx-1">
+          <a
+            href="/"
+            className="navbar-brand"
+            data-aos="fade-down"
+            data-aos-duration="1500"
+          >
+            {greeting.logo_name}
+          </a>
+          <button
+            className="navbar-toggler collapsed d-flex d-md-none justify-content-around flex-column"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="toggler-icon top-icon"></span>
+            <span className="toggler-icon mid-icon"></span>
+            <span className="toggler-icon bottom-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul
+              className="navbar-nav ms-auto"
+              data-aos="fade-left"
+              data-aos-duration="2500"
+              data-aos-delay="500"
+            >
+              {links.map((link, index) => (
+                <li className="nav-item ms-lg-3" key={index.toString()}>
+                  <Link className="nav-link px-3" to={link.to}>
+                    {link.name}{" "}
+                    <i className="fa-solid fa-arrow-right d-md-none arrow"></i>
+                  </Link>
+                </li>
+              ))}
+              <li className="nav-item ms-lg-3">
+                <Link className="nav-link px-3" to="/contact" id="contact-btn">
+                  Contact Me
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <Outlet />
+    </>
+  );
+}
+
+export default Navbar;
