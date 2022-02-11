97af57e21534a615d7b7d8e1e0e7dc8e7a55b6ee
Agathiyan <agathiyans28@gmail.com>
2022-02-11 17:13:11 +0530

    Greeting Completed with Social media links.

---
+++ src/components/greeting/Greeting.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { greeting } from "../../data";
+import { ReactComponent as Image } from "../../assets/home_image.svg";
+import "./Greeting.css";
+import SocialMedia from "./SocialMedia";
+
+function Greeting() {
+  return (
+    <div
+      className="greeting-container mx-auto"
+      id="home"
+      data-aos="fade-up"
+      data-aos-duration="2000"
+    >
+      <div className="row greeting-main">
+        <div className="col-md-6 mb-5">
+          <div className="greeting-text-div">
+            <h1 className="greeting-text-name">{greeting.title}</h1>
+            <h2 className="fs-1 greeting-text-role">{greeting.role}</h2>
+            <p className="text-muted">{greeting.subTitle}</p>
+          </div>
+          <SocialMedia />
+        </div>
+        <div className="col-md-6 mt-3">
+          <Image className="h-auto mw-100" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Greeting;
