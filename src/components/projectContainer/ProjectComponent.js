2023a16b03548052f580b7d4c8bb6f42d11e4655
Agathiyan <agathiyans28@gmail.com>
2022-02-27 16:45:24 +0530

    project component updated.

--- src/components/projectContainer/ProjectComponent.js
+++ src/components/projectContainer/ProjectComponent.js
@@ -1,48 +1,54 @@
 import React from "react";
 import "./ProjectComponent.css";
+import { project } from "../../data";
 
-function ProjectCard() {
+function ProjectCard(props) {
+  const prop = props.item;
   return (
     <div
       className="col-12 col-sm-10 col-md-6 col-xl-4 mt-5"
       // data-aos="zoom-in-up"
       // data-aos-duration="1200"
     >
-      <div class="project-card mx-auto">
-        <div
-          class="thumb"
-          // style={{
-          //   backgroundImage: `url(${
-          //     process.env.PUBLIC_URL + "/portfolio_thumb.png"
-          //   })`,
-          // }}
-          style={{ backgroundImage: `url('../../assets/images/degree.png')` }}
-        ></div>
+      <div className="project-card mx-auto">
+        <div className="thumb">
+          <img
+            className="w-100 h-100 rounded-3"
+            src={require(process.env.PUBLIC_URL +
+              "../../assets/images/" +
+              prop.thumb)}
+            alt=""
+          />
+        </div>
 
-        <div class="overlay">
-          <h2 class="title">Plant AI</h2>
-          <h3 class="date">Created on 04-02-2022</h3>
+        <div className="overlay">
+          <h2 className="title">{prop.title}</h2>
+          <h3 className="date">Created on {prop.date}</h3>
 
-          <p class="details overflow-auto">
-            Join us for our Live Infinity Session in beautiful New York City.
-            This is a 3 day intensive workshop where you'll learn how to become
-            a better version of workshop where you'll learn how to become a
-            better version of...
-          </p>
-          <h3 class="seats">
-            Language's:{" "}
-            <span className="">
-              <img
-                className="lang-logo"
-                src={require("../../assets/logo/html5.png")}
-                alt="programming language logo"
-              />
+          <p className="details overflow-auto">{prop.details}</p>
+          <h3 className="seats mt-3 mt-md-0">Language's: </h3>
+          <div className="lang-div mx-auto">
+            <span className="lang-tag d-flex justify-content-center pb-3">
+              {prop.lang.map((item, index) => (
+                <img
+                  className="lang-logo mx-2"
+                  src={require("../../assets/logo/" + item)}
+                  alt="programming language logo"
+                  key={index}
+                />
+              ))}
             </span>
-          </h3>
 
-          <a href="#" class="goto-btn btn btn-success " role="button">
-            view details
-          </a>
+            <a
+              href={prop.link}
+              target="_blank"
+              rel="noreferrer"
+              className="goto-btn btn btn-success mt-2 mt-sm-3"
+              role="button"
+            >
+              view details
+            </a>
+          </div>
         </div>
       </div>
     </div>
@@ -60,9 +66,9 @@ function ProjectComponent() {
         My Recent Works
       </h1>
       <div className="row mx-auto d-flex justify-content-center align-items-center">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        {project.map((item, index) => (
+          <ProjectCard item={item} key={index} />
+        ))}
       </div>
     </div>
   );
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -4,16 +4,17 @@ import ProjectImg from "./ProjectImg";
 import Arrow from "../../components/arrow/Arrow";
 import "./Projects.css";
 import ProjectComponent from "../../components/projectContainer/ProjectComponent";
+import Footer from "../../components/footer/Footer";
 
 function ProjectGreeting() {
   return (
     <div
-      className="project-container mx-auto"
+      className="project-container mx-auto "
       id="project"
       // data-aos="fade-up"
       // data-aos-duration="2000"
     >
-      <div className="row d-md-flex justify-content-around">
+      <div className="row d-md-flex justify-content-around mt-5">
         <div className="col-md-6 p-3 pt-5 p-md-5 img-div">
           <ProjectImg className="h-auto w-100" />
         </div>
@@ -35,6 +36,7 @@ function Projects() {
       <Navbar />
       <ProjectGreeting />
       <ProjectComponent />
+      <Footer />
     </>
   );
 }
